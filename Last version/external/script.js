2c3ee661e38f0e43c73ef153ab6d62daf5a271f9
Aya Ali <101468593+AyaA1i@users.noreply.github.com>
2023-04-13 02:18:29 +0200

    Update script.js

--- Last version/external/script.js
+++ Last version/external/script.js
@@ -34,8 +34,8 @@ function keepHighlighted() {
 
 
 }
-/*Login*/
-localStorage.setItem(20210083,"Aya");
+/* Login JS */
+localStorage.setItem(20210083,"Aya")
 function validate(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
@@ -50,4 +50,68 @@ function validate(){
         }
     }
 }
+/*Data JS*/
+function GetData(){
+    /*test */
+    const arr = ["Hany",2003,20210083,3.8,"Male","AI",2,"Active","hanyhanon@gmail.com","01234567"];
+    localStorage.setItem("Data", JSON.stringify(arr));
+    const arr2 = ["dydy",2003,20210083,3.8,"Female","AI",2,"Active","dydydodo@gmail.com","01234567"];
+    localStorage.setItem("Data2", JSON.stringify(arr2));
+    const arr3 = ["dydy",2003,20210093,3.8,"Female","AI",2,"Inactive","dydydodo2@gmail.com","01234567"];
+    localStorage.setItem("Data3", JSON.stringify(arr3));
+    /* don't forget to remove the things above me*/
+    var table = document.getElementById("Table");
+    var HtmlTable = "";
+    for(let key in localStorage){
+        if(key==20210083)continue;
+        const valuesArray = JSON.parse(localStorage.getItem(key));
+        HtmlTable += "<tr>";
+        if(valuesArray!=null){
+            for(let i=0;i<valuesArray.length;i++){
+                HtmlTable += "<td>" + valuesArray[i] + "</td>";
+            }
+            HtmlTable += "<td><a href=\"Edit.html\"><img src=\"pen3.png\" alt=\"\"></a></td>";
+            HtmlTable += "</tr>";
+        }
+    }
+    if(HtmlTable != null){
+        table.innerHTML = "<table><thead><tr><th>Name</th><th>Date</th><th>ID</th><th>GPA</th><th>Gender</th><th>Department</th><th>Level</th><th>Status</th><th>Email</th><th>Mobile</th><th></th></tr></thead>"+"<tbody>"+ HtmlTable +"</tbody></table>";
+    }
+}
+
+function filterdata(){
+    var name = document.getElementById("searchB").value;
+    var table = document.getElementById("Table");
+    table.innerHTML="";
+    var HtmlTable = "";
+    console.log(table.innerHTML);
+    for(let key in localStorage){
+        if(key==20210083)continue;
+        const valuesArray = JSON.parse(localStorage.getItem(key));
+        if(valuesArray!=null){
+           if(valuesArray[0].toLowerCase()==name.toLowerCase() && valuesArray[7]=="Active"){
+                HtmlTable += "<tr>";
+                for(let i=0;i<valuesArray.length;i++){
+                    HtmlTable += "<td>" + valuesArray[i] + "</td>";
+                }
+                HtmlTable += "<td><a href=\"Edit.html\"><img src=\"pen3.png\" alt=\"\"></a></td>";
+                HtmlTable += "</tr>";
+           }
+        }
+    }
+    console.log(table.innerHTML);
+    if(HtmlTable != null){
+        table.innerHTML = "<table><thead><tr><th>Name</th><th>Date</th><th>ID</th><th>GPA</th><th>Gender</th><th>Department</th><th>Level</th><th>Status</th><th>Email</th><th>Mobile</th><th></th></tr></thead>"+"<tbody>"+ HtmlTable +"</tbody></table>";
+    }
+
+}
+
+
+
+
+
+
+
+
+
 
