9eb897c3dbdeb01d3c19830247352a866e1bf615
MayaZayn <mayozain5@gmail.com>
2023-04-16 23:27:25 +0200

    Update Home and login pages

--- Msh Last version/external/script.js
+++ Msh Last version/external/script.js
@@ -1,23 +1,3 @@
-function autoSlideShow() {
-  let slideIndex = 0;
-  carousel();
-
-  function carousel() {
-      let x = document.getElementsByClassName("mySlides");
-      for (let i = 0; i < x.length; i++) {
-          x[i].style.display = "none";
-      }
-
-      slideIndex++;
-      if (slideIndex > x.length) {
-          slideIndex = 1
-      }
-
-      x[slideIndex - 1].style.display = "block";
-      setTimeout(carousel, 3000); // Change image every 3 seconds
-  }
-}
-
 function keepHighlighted() {
   // Get the current page URL
   var currentPageUrl = window.location.href;
